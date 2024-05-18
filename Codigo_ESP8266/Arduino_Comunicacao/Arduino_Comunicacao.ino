f2a71b787db09369d2582d70c6190421cff39f5a
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-05-18 12:50:13 -0300

    Comunicação e transmissão bem sucedida de dados dos 3 sensores

--- Codigo_ESP8266/Arduino_Comunicacao/Arduino_Comunicacao.ino
+++ Codigo_ESP8266/Arduino_Comunicacao/Arduino_Comunicacao.ino
@@ -1,18 +1,34 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial Arduino(2, 3); // Cria uma instância de comunicação serial com o Arduino
+SoftwareSerial Arduino(2, 3); // Cria uma instância de comunicação serial com o Arduino, cabo azul no D7, verde no D8
+int pinoSensor1 = A2, pinoSensor2 = A3, pinoSensor3 = A4;
 
 void setup() {
   Serial.begin(9600); // Inicia a comunicação serial com o computador (para debug)
-  Arduino.begin(4800); // Inicia a comunicação serial com o Arduino
+  Arduino.begin(4800); // Inicia a comunicação serial com o Esp8266
 }
 
 void loop() {
-  while(Arduino.available()>0){
+  
+  int terra1 = analogRead(pinoSensor1);
+  int umidade1 = map(terra1,1023,0,0,100);
+  int terra2 = analogRead(pinoSensor2);
+  int umidade2 = map(terra2,1023,0,0,100);
+  int terra3 = analogRead(pinoSensor3);
+  int umidade3 = map(terra3,1023,0,0,100);
+
+  Arduino.print(umidade1);
+  Arduino.print(","); // Separador
+  Arduino.print(umidade2);
+  Arduino.print(","); // Separador
+  Arduino.println(umidade3); // Envia um '\n' como terminador de linha
+
+
+  /*while(Arduino.available()>0){
     float val = Arduino.parseFloat();
     if(Arduino.read()=='\n'){
       Serial.println(val);
     }
-  }
-  delay(1000);
+  }*/
+  delay(5000);
 }
--- Codigo_ESP8266/Codigo_ESP8266.ino
+++ Codigo_ESP8266/Codigo_ESP8266.ino
@@ -12,9 +12,30 @@ void setup() {
 }
 
 void loop() {
-  int i = 10;
+  while(NODEMCU.available()>0){
+    
+    String receivedData = NODEMCU.readStringUntil('\n'); // Lê até o terminador de linha
+
+    int firstDelimiterIndex = receivedData.indexOf(','); // Encontra a primeira vírgula
+    int secondDelimiterIndex = receivedData.indexOf(',', firstDelimiterIndex + 1); // Encontra a segunda vírgula
+
+    if (firstDelimiterIndex != -1 && secondDelimiterIndex != -1) {
+      String val1 = receivedData.substring(0, firstDelimiterIndex); // Primeiro valor
+      String val2 = receivedData.substring(firstDelimiterIndex + 1, secondDelimiterIndex); // Segundo valor
+      String val3 = receivedData.substring(secondDelimiterIndex + 1); // Terceiro valor
+
+      Serial.println("Valores recebidos do Sensor Arduino 1: " + val1);
+      Serial.println("Valores recebidos do Sensor Arduino 2: " + val2);
+      Serial.println("Valores recebidos do Sensor Arduino 3: " + val3);
+    }
+    /*int val = NODEMCU.parseInt();
+    if(NODEMCU.read()=='\n'){
+      Serial.println("Valores recebidos do arduino: "+ String(val));
+    }*/
+  }
+  /*int i = 10;
   NODEMCU.print(i);
   NODEMCU.println("\n");
   delay(1000);
-  i++;
+  i++;*/
 }
